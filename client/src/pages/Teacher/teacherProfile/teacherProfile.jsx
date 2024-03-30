091a0089b92e9815713d244f96362783b1c1ab5d
Snehil <snehil.sharma020901@gmail.com>
2024-03-30 21:17:37 +0530

    fix#5

--- client/src/pages/Teacher/teacherProfile/teacherProfile.jsx
+++ client/src/pages/Teacher/teacherProfile/teacherProfile.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Typography, Button, Avatar, Box } from "@mui/material";
 // import StudentList from "./studentList";
-import "./TeacherProfile.css"; // Import the CSS file
+import "./teacherProfile.css"; // Import the CSS file
 
 const TeacherProfile = () => {
   const [isClassTeacher, setIsClassTeacher] = useState(true);
