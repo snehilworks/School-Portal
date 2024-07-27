d8c160fa3be7bbeb8d542f3b6a0af169175a2cb0
Snehil <snehil.sharma020901@gmail.com>
2024-07-28 03:18:37 +0530

    classes get api for home routes

--- api/routes/homeRoutes.ts
+++ api/routes/homeRoutes.ts
@@ -3,10 +3,8 @@ import * as homeController from "./../controller/homeController";
 
 const router = express.Router();
 
-router.get("/admissions", homeController.getAdmissions);
-
 router.post("/contact", homeController.contactMessage);
 
-// router.get("/academics-activities", homeController.getActivities);
+router.get('/classes', homeController.getClasses);
 
 export default router;
