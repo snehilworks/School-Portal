5e32ded359b80833c94f3c0051436670db55815a
Snehil <snehil.sharma020901@gmail.com>
2024-08-05 03:50:55 +0530

    field Type and feeType logical fixing: Payment Model

--- api/models/PaymentModel.ts
+++ api/models/PaymentModel.ts
@@ -5,7 +5,8 @@ interface IPayment extends Document {
   paymentId: string;
   studentId: mongoose.Schema.Types.ObjectId;
   studentName: string;
-  feeType: string;
+  fieldType: 'ADMISSION' | 'FEES' | 'HOSTEL';
+  feeType?: 'ANNUAL' | 'HALF-YEARLY' | 'MONTHLY';
   studentClass: mongoose.Schema.Types.ObjectId;
   amount: number;
   paymentStatus: string;
@@ -17,11 +18,29 @@ const paymentSchema: Schema = new Schema({
   paymentId: { type: String, required: true },
   studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   studentName: { type: String, required: true },
-  feeType: { type: String, required: true }, // { annual , 6-month, monthly }
+  fieldType: {
+    type: String,
+    required: true,
+    enum: ['ADMISSION', 'FEES', 'HOSTEL']
+  },
+  feeType: {
+    type: String,
+    enum: ['ANNUAL', 'HALF-YEARLY', 'MONTHLY']
+  },
   studentClass: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
   amount: { type: Number, required: true },
   paymentStatus: { type: String, required: true },
   paymentDate: { type: Date, default: Date.now },
+});
+
+// Pre-save hook for conditional validation
+paymentSchema.pre('save', function (next) {
+  if (this.fieldType === 'fees' && !this.feeType) {
+    // `this` refers to the current document
+    next(new Error('feeType is required if fieldType is fees'));
+  } else {
+    next();
+  }
 });
 
 const Payment = mongoose.model<IPayment>('Payment', paymentSchema);
