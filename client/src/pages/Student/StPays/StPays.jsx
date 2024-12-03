7850f498aab2c112078cedc97d0ef01531c8b926
Snehil <snehil.sharma020901@gmail.com>
2024-12-03 16:20:27 +0530

    annual fee payment fixed and UI changed!

--- client/src/pages/Student/StPays/StPays.jsx
+++ client/src/pages/Student/StPays/StPays.jsx
@@ -5,6 +5,7 @@ import PrimaryButton from "../../../components/ui/PrimaryButton";
 import axiosInstance from "../../../utils/axiosInstance";
 import ErrorModal from "../../../components/ErrorModal";
 import InternalServerModal from "../../../components/InternalServerModal";
+import { IoArrowBackOutline } from "react-icons/io5";
 
 const PaymentsPage = () => {
   const [amount, setAmount] = useState("");
@@ -114,92 +115,95 @@ const PaymentsPage = () => {
     try {
       const response = await axiosInstance.post(`/api/pay/order`, {
         amount: finalAmount,
-        currency: "INR",
-        receipt: `OrderReceipt1_${startDate
-          .toISOString()
-          .slice(0, 10)}_${endDate.toISOString().slice(0, 10)}`,
       });
       const order = response.data;
-
       if (response.status === 512) {
         setInternalServerError(
           "Internal Server Error. Please try again later."
         );
         return; // Exit early
       }
-
-      const options = {
-        key: import.meta.env.VITE_RAZORPAY_KEY || "",
-        amount: order.amount,
-        currency: "INR",
-        name: "Shivam Public",
-        description: "School Fee Payment",
-        image: "https://example.com/your_logo",
-        order_id: order.id,
-        handler: async function (response) {
-          // Verify the payment
-          try {
-            const verifyResponse = await axiosInstance.post(
-              `/api/pay/verify-payment`,
-              {
-                orderId: order.id,
-                paymentId: response.razorpay_payment_id,
-                signature: response.razorpay_signature,
-                studentId: studentId,
-                studentName: name,
-                fieldType: "FEES",
-                feeType: feeType.toUpperCase().replace(" ", "-"), // Convert to the required format
-                studentClass: studentClass,
-                amount: finalAmount / 100, // Convert back to INR
-                paymentDate: new Date(),
-              }
-            );
-
-            if (verifyResponse.status === 201) {
-              // Navigate to another page or show a success message
-              navigate("/student/payment-completion", {
-                state: { paymentId: response.razorpay_payment_id },
-              });
-            }
-          } catch (error) {
-            console.error("Error verifying payment:", error);
-            setError("Payment verification failed. Please contact support.");
-          }
-        },
-        prefill: {
-          name: name,
-          email: email,
-          contact: fatherName,
-        },
-        notes: {
-          address: "",
-        },
-        theme: {
-          color: "#3399cc",
-        },
-      };
-
-      const rzp1 = new window.Razorpay(options);
-      rzp1.on("payment.failed", function (response) {
-        alert(`Payment failed: ${response.error.description}`);
-      });
-      rzp1.open();
+      VerifyPayment(order, finalAmount);
     } catch (error) {
       console.error("Error initiating payment:", error);
     }
   };
 
+  const VerifyPayment = async (data, finalAmount) => {
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY || "",
+      amount: data.amount,
+      currency: "INR",
+      name: "Shivam Public, Aarni",
+      description: "School Fee Payment",
+      image: "https://example.com/your_logo",
+      order_id: data.id,
+      handler: async function (response) {
+        // Verify the payment
+        try {
+          const verifyResponse = await axiosInstance.post(
+            `/api/pay/verify-payment`,
+            {
+              orderId: data.id,
+              paymentId: response.razorpay_payment_id,
+              signature: response.razorpay_signature,
+              studentId: studentId,
+              studentName: name,
+              fieldType: "FEES",
+              feeType: feeType.toUpperCase().replace(" ", "-"), // Convert to the required format
+              studentClass: studentClass,
+              amount: finalAmount / 100, // Convert back to INR
+              paymentDate: new Date(),
+            }
+          );
+
+          if (verifyResponse.status === 201) {
+            navigate("/student/payment-completion", {
+              state: { paymentId: response.razorpay_payment_id },
+            });
+          }
+        } catch (error) {
+          console.error("Error verifying payment:", error);
+          setError("Payment verification failed. Please contact support.");
+        }
+      },
+      prefill: {
+        name: name,
+        email: email,
+        contact: fatherName,
+      },
+      notes: {
+        address: "",
+      },
+      theme: {
+        color: "#115E59",
+      },
+    };
+
+    var rzp1 = new window.Razorpay(options);
+    rzp1.on("payment.failed", function (response) {
+      alert(`Payment failed: ${response.error.description}`);
+    });
+    rzp1.open();
+  };
+
   return (
-    <div className="min-h-screen bg-gradient-to-br from-purple-400 to-blue-500 flex flex-col items-center py-8 px-4 sm:px-6 lg:px-8 xl:px-16">
-      <button
-        onClick={() => navigate(-1)}
-        className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-md mb-8 hover:bg-blue-700 transition duration-200"
-      >
-        ← Back
-      </button>
-      <div className="max-w-3xl w-full bg-white rounded-lg shadow-lg p-8 space-y-6 mx-auto">
+    <div className="min-h-screen bg-gradient-to-r from-gray-100 to-teal-300 flex flex-col items-center py-6 px-4 sm:px-6 lg:px-8 xl:px-16">
+      <div className="max-w-3xl w-full bg-white rounded-lg shadow-lg p-8 mx-auto">
+        <button
+          onClick={() => navigate(-1)}
+          className="absolute bg-gray-400 text-black shadow-md font-bold px-2 py-1.5 text-sm md:py-2 md:px-5 rounded-md hover:bg-gray-500 transition duration-200"
+        >
+          <span className="block md:hidden px-3 py-1">
+            <IoArrowBackOutline className="text-md" />
+          </span>
+          <span className="hidden md:flex md:gap-1">
+            <IoArrowBackOutline className="mt-0.3 text-lg" /> Back
+          </span>
+        </button>
+
         <div className="text-center mb-6">
-          <Payment className="text-6xl text-blue-600 mb-4" />
+          <Payment className="text-6xl font-bold text-blue-600 mb-4" />
           <h2 className="text-3xl font-extrabold text-gray-900">
             School Fee Payment
           </h2>
@@ -209,46 +213,52 @@ const PaymentsPage = () => {
         </div>
 
         <form onSubmit={handlePayment} className="space-y-6">
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Student Name
-            </label>
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="mt-1 block w-full px-4 py-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-base"
-              required
-            />
-          </div>
+          <div className="flex flex-col md:flex-row justify-around gap-2">
+            <div className="w-full">
+              <label className="block text-sm font-medium text-gray-700">
+                Student Name
+              </label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="mt-1 block w-full px-4 py-2 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-base"
+                required
+              />
+            </div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Father's Name
-            </label>
-            <input
-              type="text"
-              value={fatherName}
-              onChange={(e) => setFatherName(e.target.value)}
-              className="mt-1 block w-full px-4 py-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-base"
-              required
-            />
-          </div>
-
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Student Class
-            </label>
-            <div className="mt-1 p-4 bg-blue-100 border-l-4 border-blue-500 text-blue-700 rounded-md">
-              {description}
+            <div className="w-full">
+              <label className="block text-sm font-medium text-gray-700">
+                Father's Name
+              </label>
+              <input
+                type="text"
+                value={fatherName}
+                onChange={(e) => setFatherName(e.target.value)}
+                className="mt-1 block w-full px-4 py-2 rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-base"
+                required
+              />
             </div>
           </div>
 
-          {amount && (
-            <div className="p-4 bg-blue-100 border-l-4 border-blue-500 text-blue-700 rounded-md">
-              <p>Fee Amount for Class: INR {amount}</p>
+          <div className="flex flex-col md:flex-row justify-around items-center gap-2">
+            <div className="w-full">
+              <label className="block text-sm font-medium text-gray-700">
+                Student Class
+              </label>
+              <div className="mt-1 p-4 bg-blue-100 border-l-4 border-blue-500 text-blue-700 rounded-md">
+                {description}
+              </div>
             </div>
-          )}
+
+            {amount && (
+              <div className="w-full">
+                <div className="p-4 md:mt-5 bg-blue-100 border-l-4 border-blue-500 text-blue-700 rounded-md">
+                  <p>Fee Amount for Class: INR {amount}</p>
+                </div>
+              </div>
+            )}
+          </div>
 
           <div>
             <label className="block text-sm font-medium text-gray-700">
@@ -290,8 +300,13 @@ const PaymentsPage = () => {
             />
           </div>
 
-          <div className="text-center">
-            <PrimaryButton type="submit">Pay Now</PrimaryButton>
+          <div className="flex items-center justify-center text-center">
+            <PrimaryButton
+              className={"!px-14 !py-2 !bg-teal-700  "}
+              type="submit"
+            >
+              Pay Now
+            </PrimaryButton>
           </div>
         </form>
       </div>
